aa2649b121a23e4b94b4e5509a04a2c149375986
matrix-man-12 <yogeshyadav32321212@gmail.com>
2022-07-21 21:22:14 +0530

    3 mini-projects

--- canvas.js
+++ canvas.js
@@ -1,6 +1,7 @@
 
 let canvas1 = document.querySelector('.canvas-1')
 let canvas2 = document.querySelector('.canvas-2')
+let canvas3 = document.querySelector('.canvas-3')
 
 canvas1.width = window.innerWidth 
 canvas1.height = window.innerHeight
@@ -9,6 +10,10 @@ let context1 = canvas1.getContext('2d')
 canvas2.width = window.innerWidth 
 canvas2.height = window.innerHeight
 let context2 = canvas2.getContext('2d')
+
+canvas3.width = window.innerWidth 
+canvas3.height = window.innerHeight
+let context3 = canvas3.getContext('2d')
 
 // fillRect(x, y, width, height)
 context1.fillStyle = '#73A9AD'
@@ -28,7 +33,7 @@ context1.lineTo(400, 500)
 context1.strokeStyle = '#576F72'
 context1.stroke()
 
-// // line-2
+// line-2
 context1.beginPath()
 context1.moveTo(200, 100)
 context1.lineTo(500, 400)
@@ -44,25 +49,10 @@ context1.strokeStyle = '#ABC9FF'
 context1.stroke()
 
 
-// function randomize(){
-//     // clear the canvas
-//     context1.clearRect(0, 0, canvas1.width, canvas1.height)
-//     for(let i = 0; i < 50; i++){
-//         let x = Math.random() * window.innerWidth
-//         let y = Math.random() * window.innerHeight
-//         let radius = Math.random() * 100
-//         const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    
-//         context1.beginPath()
-//         context1.arc(x, y, radius, 0, Math.PI * 2, false)
-//         context1.strokeStyle = `#${randomColor}`
-//         context1.stroke()
-//     }
-// }
-
-// randomize()
 
 
+
+// Canvas 2
 function Circle(x, y, dx, dy, radius, color) {
     this.x = x
     this.y = y
@@ -74,9 +64,9 @@ function Circle(x, y, dx, dy, radius, color) {
     this.draw = function() {
         context2.beginPath()
         context2.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        context2.strokeStyle = this.color
+        context2.fillStyle = this.color
         // console.log(`Color is: ${this.color}`);
-        context2.stroke()
+        context2.fill()
     }
 
     this.update = function() {
@@ -125,3 +115,105 @@ function animate() {
 }
 
 animate()
+
+
+// Canvas 3
+
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+let maxRadius = 50
+// let minRadius = 10
+
+canvas3.addEventListener("mousemove", (event) => {
+    mouse.x = event.x
+    mouse.y = event.y
+})
+
+window.addEventListener("resize", (event) => {
+    canvas3.width = window.innerWidth
+    canvas3.height = window.innerHeight
+
+    init()
+})
+
+let colorArray = ["#495C83", "#7A86B6", "#C4D7E0", "#A2B5BB"]
+
+function Circle3(x, y, dx, dy, radius,) {
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = radius
+    this.minRadius = radius
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
+    
+    this.draw = function() {
+        context3.beginPath()
+        context3.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context3.fillStyle = this.color
+        context3.fill()
+    }
+
+    this.update = function() {
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx
+        }
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy
+        }
+        this.x += this.dx
+        this.y += this.dy
+
+
+        // mouse hover growth
+        if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if(this.radius < maxRadius) {
+                this.radius += 1
+            }
+        } else if(this.radius > this.minRadius) {
+            this.radius -= 1
+        }         
+
+        this.draw()
+    }
+}
+
+
+let circleArray3 = []
+
+let numberOfCircles3 = 900
+
+function init() {
+    circleArray3 = []
+    for(let i = 0; i < numberOfCircles3; i++ ) {
+        
+        let radius = (Math.random() * 3 + 1)
+
+        let x = Math.random() * (window.innerWidth - radius*2) + radius
+        let y = Math.random() * (window.innerHeight - radius*2) + radius
+
+        let dx = (Math.random() - 0.5) * 5
+        let dy = (Math.random() - 0.5 ) * 5
+        
+        let circle = new Circle3(x, y, dx, dy, radius,)
+        
+        circleArray3.push(circle)
+    }
+}
+
+init()
+
+function animate3() {
+    requestAnimationFrame(animate3)
+
+    context3.clearRect(0, 0, canvas3.width, canvas3.height)
+
+    for(let i = 0; i < circleArray3.length; i++) {
+        circleArray3[i].update()
+    }
+}
+
+animate3()
